8ed7251ebcdb72dd696121257b8bbe1c49e5c4c4
Catriel Müller <catrielmuller@gmail.com>
2017-01-08 15:57:25 -0300

    Refactor Substring

--- subString.py
+++ subString.py
@@ -1,16 +1,8 @@
-def subs(a):
+def subs(input_string):
     """
     Generates a list of substrings
     """
-    x = len(a)
-    ls = []
+    length = len(input_string)
+    return [input_string[i:j+1] for i in xrange(length) for j in xrange(i,length)]
 
-    for i in range(x): # 0 - 3
-        ls.append(a[i:]) # 0 hf
-        j = len(a[i:])# 2
-        for k in range(j): # 0 - 2
-            ls.append(a[i:j]) # 0 - 2
-            j -= 1 # 2-1=1
-    return ls
-
-print subs('1234')
+print subs('123454984')
