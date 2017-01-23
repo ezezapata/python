c8dad4b5e26459c5b9acae3b173019d444b2c2c2
ezezapata <zapata.ezequiel@gmail.com>
2017-01-23 11:54:05 +0200

    Create contactslist.py

---
+++ contactslist.py
@@ -0,0 +1,6 @@
+"""
+    This program generates a list of contacts in which can create a new one, delete it or modify it.
+Each contact can have a phone number, email adress and an adress too.
+"""
+
+contacts = {} # Store the contacts in this dictionary
